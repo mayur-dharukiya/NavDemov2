6b90e3dd82d71a4cb63734327cfca263cd9a4306
mayur-dharukiya <mayur@revature.com>
2022-03-15 15:13:14 -0400

    one more

--- app/src/main/java/com/example/navdemov2/MainActivity.kt
+++ app/src/main/java/com/example/navdemov2/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.navdemov2
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
@@ -11,12 +13,18 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.navdemov2.ui.theme.NavDemov2Theme
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,7 +45,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 @Composable
-fun LoginScreen()
+fun LoginScreen(navController: NavController)
 {
     //Text-LoginScreen
     //Button-Go to Profile
@@ -56,19 +64,30 @@ fun LoginScreen()
 
 }
 
+
+
 @Composable
 fun ProfileScreen(navController: NavController,name:String,userId:String,created:Long)
 {
     //Text- Data from Login Screen
     //Button-Go to Post screen
 
+    val user = rememberSaveable{
+        User(
+            name = name,
+            id = userId,
+            created = LocalDateTime.ofInstant(
+                Instant.ofEpochMilli(created), ZoneId.systemDefault()
+            )
+        )
+    }
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     )
     {
-        Text(text = "Profile Screen")
+        Text(text = "Profile Screen:$user",textAlign= TextAlign.Center)
         Button(onClick = { /*TODO*/ }) {
 
             Text(text = "Go to Post  Screen")
@@ -77,8 +96,12 @@ fun ProfileScreen(navController: NavController,name:String,userId:String,created
 }
 
 @Composable
-fun PostScreen()
+fun PostScreen(showPost:Boolean=false)
 {
-    //Text-Post Screen- True(data from previous screen)
+    Box(modifier = Modifier.fillMaxSize(),
+    contentAlignment = Alignment.Center)
+    {
+        Text(text = "Post Screen $showPost")
+    }
 }
 
--- app/src/main/java/com/example/navdemov2/MyUser.kt
+++ app/src/main/java/com/example/navdemov2/MyUser.kt
@@ -6,17 +6,9 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 import java.time.LocalDateTime
 
-
-
-@Parcelize
-data class User (
-
+data class MyUser(
     val name:String,
     val id:String,
-    val created:LocalDateTime
+    val created: LocalDateTime
 
-    ):Parcelable
-
-
-
-
+): Parcelable
